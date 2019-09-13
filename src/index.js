85aa4f4d294f2fea8819f3d8a3ff7be7b8968390
charmming <charmming4u@gmail.com>
2019-09-13 15:46:10 +0100

    added new babel files and watch command

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let age = 7;
+let name = "Muhammed";
+
+let newName = "Tomma mamma";
+
+const print = () => {
+  console.log(name + age);  
+};
+
+
+class user{
+    register(){
+        console.log('User Registered');
+    }
+}
